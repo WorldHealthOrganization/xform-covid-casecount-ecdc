765abbb1860e1f873f444d2e0d4a3964a62da005
Johannes Schnitzler <67371775+schnitzlerjc@users.noreply.github.com>
2021-02-03 11:24:24 +0100

    Update xform.R

--- src/xform.R
+++ src/xform.R
@@ -23,12 +23,12 @@ data <- suppressMessages(readr::read_csv(tf, na = "")) %>%
 message("Test message1")
 # needs to be daily - fill in other days with zeros
 data <- data %>%
-  dplyr::group_by(admin0_code) %>%
+#JS  dplyr::group_by(admin0_code) %>%
   tidyr::complete(
     date = seq.Date(min(date), max(date), by = "day"),
     fill = list(cases = 0, deaths = 0)) %>%
   tidyr::fill(admin0_code) %>%
-  ungroup()
+#JS  ungroup()
 message("Test message2")
 message("Most recent date: ", max(data$date))
 
