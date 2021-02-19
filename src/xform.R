78e1086e7a39a061e866a8e757290b0a19589f2f
Johannes Schnitzler <67371775+schnitzlerjc@users.noreply.github.com>
2021-02-19 18:29:08 +0100

    Update xform.R

--- src/xform.R
+++ src/xform.R
@@ -1,7 +1,10 @@
 suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(httr))
+suppressPackageStartupMessages(library(lubridate))
+
 
 message("Test message")
+
 
 dir.create("output", showWarning = FALSE)
 dir.create("output/admin0", showWarning = FALSE)
@@ -11,6 +14,7 @@ httr::GET("https://opendata.ecdc.europa.eu/covid19/nationalcasedeath/csv",
   httr::authenticate(":", ":", type = "ntlm"),
   httr::write_disk(tf <- tempfile(fileext = ".csv")))
 
+if(FALSE){
 data <- suppressMessages(readr::read_csv(tf, na = "")) %>%
   dplyr::mutate(dateRep = as.Date(dateRep, format = "%d/%m/%Y")) %>%
   dplyr::select(-year_week, -countryterritoryCode,
@@ -21,7 +25,27 @@ data <- suppressMessages(readr::read_csv(tf, na = "")) %>%
     "International Conveyance", admin0_code)) %>%
   dplyr::select(admin0_code, date, cases, deaths) %>%
   dplyr::arrange(admin0_code, date)
+}
 
+### Changed ECDC structure and missing ISO2 2020-02-19 START
+ISO_2_3 <- data.frame(
+ISO3 = c("AFG","ALB","DZA","AND","AGO","AIA","ATG","ARG","ARM","ABW","AUS","AUT","AZE","BHS","BHR","BGD","BRB","BLR","BEL","BLZ","BEN","BMU","BTN","BOL","BES","BIH","BWA","BRA","VGB","BRN","BGR","BFA","BDI","CPV","KHM","CMR","CAN","CYM","CAF","TCD","CHL","CHN","COL","COM","COG","CRI","CIV","HRV","CUB","CUW","CYP","CZE","COD","DNK","DJI","DMA","DOM","ECU","EGY","SLV","GNQ","ERI","EST","SWZ","ETH","FLK","FRO","FJI","FIN","FRA","PYF","GAB","GEO","DEU","GHA","GIB","GRC","GRL","GRD","GUM","GTM","GGY","GIN","GNB","GUY","HTI","HND","HUN","ISL","IND","IDN","IRN","IRQ","IRL","IMN","ISR","ITA","JAM","JPN","JEY","JOR","KAZ","KEN","KWT","KGZ","LAO","LVA","LBN","LSO","LBR","LBY","LIE","LTU","LUX","MDG","MWI","MYS","MDV","MLI","MLT","MHL","MRT","MUS","MEX","FSM","MDA","MCO","MNG","MNE","MSR","MAR","MOZ","MMR","NAM","NPL","NLD","NCL","NZL","NIC","NER","NGA","MKD","MNP","NOR","OMN","PAK","PSE","PAN","PNG","PRY","PER","PHL","POL","PRT","PRI","QAT","ROU","RUS","RWA","KNA","LCA","VCT","SMR","STP","SAU","SEN","SRB","SYC","SLE","SGP","SXM","SVK","SVN","SLB","SOM","ZAF","KOR","SSD","ESP","LKA","SDN","SUR","SWE","CHE","SYR","TWN","TJK","TZA","THA","GMB","VAT","TLS","TGO","TTO","TUN","TUR","TCA","UGA","UKR","ARE","GBR","USA","URY","VIR","UZB","VUT","VEN","VNM","WLF","ESH","YEM","ZMB","ZWE"),
+ISO2 = c("AF","AL","DZ","AD","AO","AI","AG","AR","AM","AW","AU","AT","AZ","BS","BH","BD","BB","BY","BE","BZ","BJ","BM","BT","BO","BQ","BA","BW","BR","VG","BN","BG","BF","BI","CV","KH","CM","CA","KY","CF","TD","CL","CN","CO","KM","CG","CR","CI","HR","CU","CW","CY","CZ","CD","DK","DJ","DM","DO","EC","EG","SV","GQ","ER","EE","SZ","ET","FK","FO","FJ","FI","FR","PF","GA","GE","DE","GH","GI","EL","GL","GD","GU","GT","GG","GN","GW","GY","HT","HN","HU","IS","IN","ID","IR","IQ","IE","IM","IL","IT","JM","JP","JE","JO","KZ","KE","KW","KG","LA","LV","LB","LS","LR","LY","LI","LT","LU","MG","MW","MY","MV","ML","MT","MH","MR","MU","MX","FM","MD","MC","MN","ME","MS","MA","MZ","MM","NA","NP","NL","NC","NZ","NI","NE","NG","MK","MP","NO","OM","PK","PS","PA","PG","PY","PE","PH","PL","PT","PR","QA","RO","RU","RW","KN","LC","VC","SM","ST","SA","SN","RS","SC","SL","SG","SX","SK","SI","SB","SO","ZA","KR","SS","ES","LK","SD","SR","SE","CH","SY","TW","TJ","TZ","TH","GM","VA","TL","TG","TT","TN","TR","TC","UG","UA","AE","UK","US","UY","VI","UZ","VU","VE","VN","WF","EH","YE","ZM","ZW")
+)
+
+data <- suppressMessages(readr::read_csv(tf, na = "")) %>% 
+  dplyr::mutate(dateRep = as.Date(dateRep, format = "%d/%m/%Y")) %>%
+  dplyr::select(-year_week, -countryterritoryCode,
+    -countriesAndTerritories, -popData2019) %>%
+  dplyr::rename(date = "dateRep", cases = "cases_weekly",
+    deaths = "deaths_weekly", admin0_code = "geoId") %>%
+  dplyr::mutate(admin0_code = ifelse(admin0_code == "JPG11668",
+    "International Conveyance", admin0_code)) %>%
+  dplyr::select(admin0_code, date, cases, deaths) %>%
+  dplyr::arrange(admin0_code, date)
+
+
+### Changed ECDC structure and missing ISO2 2020-02-19 END
 message("Test message1")
 message(dim(data)[1])
 
